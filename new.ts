83cf5d9ab8bf417684e2560757692c48f3884d76
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-03-10 01:03:22 +0500

    Add files via upload

---
+++ new.ts
@@ -0,0 +1,6 @@
+
+let famous_person:string = ("Eleanor Roosevelt")
+let quote:string = ("Do one thing every day that scares you.")
+let message:string = `${famous_person} onesaid, "${quote}"`
+
+console.log(message)
